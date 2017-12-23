b0088b5ed2b3c177cc34fb27ca87ce0dc779957b
Shalitha <shalithasuranga@gmail.com>
2017-12-23 11:25:25 +0530

    Changed text

--- global.config.php
+++ global.config.php
@@ -8,8 +8,8 @@ date_default_timezone_set("Asia/Colombo");
 include(__DIR__."/classes/livewishes.class.php"); 
 include(__DIR__."/classes/player.class.php"); 
  
-define("LW_APP_NAME","Live Wishes");
-define("LW_APP_URL","http://localhost/livewishes/");
-define("LW_APP_WISH","Happy Open Sourcing!");
+define("LW_APP_NAME","Live Wishes XMAS");
+define("LW_APP_URL","http://localhost/livewishesxmas/");
+define("LW_APP_WISH","Merry Christmas");
  
 ?>
